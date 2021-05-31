d7057e9ebc02ba14b5f817011077c8b427ae440a
rescobar9 <67067784+rescobar9@users.noreply.github.com>
2021-05-30 20:10:01 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+})
